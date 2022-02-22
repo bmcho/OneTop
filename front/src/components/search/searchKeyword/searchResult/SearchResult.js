4f109f32fa96026c72041b2a9629a397d5e8cc16
hyunhaha <gusgk2658@naver.com>
2022-02-23 04:43:05 +0900

    feat: search result item 구현

--- front/src/components/search/searchKeyword/searchResult/SearchResult.js
+++ front/src/components/search/searchKeyword/searchResult/SearchResult.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import SearchResultItem from '../../searchResultItem/SearchResultItem';
 
 const SearchResult = (props) => {
   const { searchResultData } = useSelector(state => state.searchKeyword);
   return (
     <div>{searchResultData && (
       <div>
-        {searchResultData.map(show => (
-          <div key={show.id}>
-            <a href={show.url}>{show.name}</a>
-            <div>점수 : {show.score}</div>
-            <div>타입 : {show.type}</div>
-            <div>언어 : {show.language}</div>
-          </div>
+        {searchResultData.map((show, idx) => (
+          <SearchResultItem key={idx} show={show} />
         ))}
       </div>
     )}
---
+++ front/src/components/search/searchResultItem/SearchResultItem.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components';
+import { theme } from '../../../../styles/theme';
+
+const SearchResultItem = ({ show }) => (
+  <SearchResultItemBlock>
+    <ImageWrap>
+      <img src='/images/product.jpeg' width={60} height={60} />
+    </ImageWrap>
+    <div>
+      <Description>이니스프리</Description>
+      <p>{show.name}</p>
+      <p>타입 : {show.type}</p>
+      <p>언어 : {show.score}</p>
+      <Description>{show.language}</Description>
+    </div>
+  </SearchResultItemBlock>
+);
+const SearchResultItemBlock = styled.div`
+display: flex;
+padding:16px 0;
+border-bottom:1px solid ${theme.color.lightGray3};
+`
+const ImageWrap = styled.div`
+display: flex;
+align-items: center;
+padding-right:16px;
+`
+const Description = styled.p`
+font-size: 12px;
+line-height:16px;
+color:${theme.color.gray2};
+margin-bottom:4px;
+`
+export default SearchResultItem;
