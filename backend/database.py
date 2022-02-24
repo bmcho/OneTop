0123ae69bf31f7d99434aa929705565d53a96386
김민석 <minseok@gimminseogs-MacBook-Air.local>
2022-02-25 00:34:49 +0900

    lint 설정을 통한 .py파일들 linting 완료

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,16 @@
-import secrets
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 import json
 import os
 
-BASE_DIR=os.path.dirname(os.path.abspath(__file__))
-SECRET_FILE=os.path.join(BASE_DIR,'secrets.json')
-secrets=json.loads(open(SECRET_FILE).read())
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DB=secrets["DB"]
-env=os.environ
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SECRET_FILE = os.path.join(BASE_DIR, ".vscode/secrets.json")
+secrets = json.loads(open(SECRET_FILE).read())
+
+DB = secrets["DB"]
+env = os.environ
 
 # user=os.getenv("MYSQL_USER")
 # password=os.getenv("MYSQL_PASSWORD")
@@ -18,13 +18,11 @@ env=os.environ
 # port=os.getenv("MYSQL_PORT",default=3306)
 # database=os.getenv("MYSQL_DATABASE")
 
-DB_URL=f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8"
-#DB_URL=f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8"
+DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8"  # noqa
+# DB_URL=f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}?charset=utf8"
 
-engine=create_engine(
-    DB_URL,encoding='utf-8'
-)
+engine = create_engine(DB_URL, encoding="utf-8")
 
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base=declarative_base()
+Base = declarative_base()
--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
+
 from apps import models
+from apps.routers import detail, home, search
 from database import engine
-from apps.routers import detail,home,search
 
-
-app=FastAPI()
+app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(detail.router)
 app.include_router(home.router)
 app.include_router(search.router)
-
