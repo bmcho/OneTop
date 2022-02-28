67a556d0280b5132a977c67c59b49230a317a763
rmsqor103 <rmsqor103@naver.com>
2022-02-28 12:19:34 +0900

    fixed: keyword redux error

--- front/src/stores/modules/index.js
+++ front/src/stores/modules/index.js
@@ -3,13 +3,14 @@ import { HYDRATE } from 'next-redux-wrapper';
 import tvShow from './tvShow';
 import searchKeyword from './searchKeyword';
 import searchTypeTap from './searchTypeTap';
-
+import loading from './loading';
 import productInfo from './productInfo';
 
 const combineReducer = combineReducers({
   tvShow,
   searchKeyword,
   searchTypeTap,
+  loading,
   productInfo,
 });
 
