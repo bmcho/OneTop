87ff8dac284f237b73f128e6655540f2662aeb2a
bmcho91 <bmcho91@naver.com>
2022-03-06 00:44:22 +0900

    fix: etl_product,models / docs:.gitignore add & fix

--- datamigration/etl_product.py
+++ datamigration/etl_product.py
@@ -37,8 +37,15 @@ class Product(BaseClass):
         if len(data) != 0 :
           continue
         
-        cursor.execute(f'INSERT INTO product (product_num, name, img_url, brand, average_rating, price, extinction) \
-                   VALUES ("{row["product_num"]}","{row["name"]}","{row["img_url"]}","{row["brand"]}", "{row["average_rating"]}", "{row["price"]}", 1)')
+        #걸러내는 규칙이 간단하기 때문에 Regex보다 split과 contain을 사용하여 처리하는것이 효율적 
+        # extinction = 0 if 단종포함 else 1
+        # price ->  capacity / price
+        extinction = 0 if "단종" in row["name"] else 1
+        price_split = row["price"].split("/")
+        price = price_split[1].translate(price_split[1].strip().maketrans({ '원':'' , ',':'' }))
+
+        cursor.execute(f'INSERT INTO product (product_num, name, img_url, brand, average_rating, capaciry, price, extinction) \
+                   VALUES ("{row["product_num"]}","{row["name"]}","{row["img_url"]}","{row["brand"]}", "{row["average_rating"]}", "{price_split[0]}", {price}, {extinction})')
         
       con.commit()
       print(f'product table - commit()')
