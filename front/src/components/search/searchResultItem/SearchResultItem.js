01cffac5ddee58690ce213f74732f0b974a1f9bb
hyunhaha <gusgk2658@naver.com>
2022-03-06 17:14:30 +0900

    feat: search result item prop 수정

--- front/src/components/search/searchResultItem/SearchResultItem.js
+++ front/src/components/search/searchResultItem/SearchResultItem.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import StarRating from '../../commons/starRating/StarRating';
-import Image from 'next/image';
 
 const SearchResultItem = ({ cosmetic }) => (
   <SearchResultItemBlock>
     <ImageWrap>
-      <Image src={cosmetic.img_url} width={60} height={60} />
+      <img src="/images/product.jpeg" width={60} height={60} />
     </ImageWrap>
     <div>
-      <Description>이니스프리</Description>
-      <p>{cosmetic.name}</p>
-      <p>브랜드 : {cosmetic.brand}</p>
-      <p>평점 : {cosmetic.average_rating}</p>
-      {/* <p>{cosmetic.score * 10}</p> */}
-      <StarRating rating={(cosmetic.average_rating * 10).toPrecision(2)} />
-      {/* <Description>{cosmetic.language}</Description> */}
+      <Description>{cosmetic.brand}</Description>
+      <Name>{cosmetic.name}</Name>
+      <StarRating rating={cosmetic.average_rating} />
+      <Description>{cosmetic.price}</Description>
     </div>
   </SearchResultItemBlock>
 );
-const SearchResultItemBlock = styled.div`
+const SearchResultItemBlock = styled.li`
   display: flex;
   padding: 16px 0;
   border-bottom: 1px solid ${(props) => props.theme.color.lightGray3};
@@ -33,6 +29,9 @@ const Description = styled.p`
   font-size: 12px;
   line-height: 16px;
   color: ${(props) => props.theme.color.gray2};
+  margin-bottom: 4px;
+`;
+const Name = styled.p`
   margin-bottom: 4px;
 `;
 export default SearchResultItem;
