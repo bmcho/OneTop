1abf956e3afbe26e029726fc11815010fd7dee1d
hyunhaha <gusgk2658@naver.com>
2022-03-09 09:35:48 +0900

    feat: search keyword sort 추가

--- front/src/components/search/searchKeyword/searchResult/SearchResult.js
+++ front/src/components/search/searchKeyword/searchResult/SearchResult.js
@@ -6,6 +6,7 @@ import {
   setRequestDataAction,
   setRequestPageAction,
   setResultTypeAction,
+  setSortAction,
 } from '../../../../stores/modules/searchKeyword';
 import Pagination from '../../../commons/pagination/Pagination';
 import SearchResultItem from '../../searchResultItem/SearchResultItem';
@@ -48,10 +49,32 @@ const SearchResult = (props) => {
     );
   };
 
+  const changeSort = (e) => {
+    const newSort = e.target.value;
+    dispatch(setSortAction(newSort));
+    dispatch(setRequestPageAction(0));
+    dispatch(
+      setRequestDataAction({
+        requestPage: 0,
+        sort: newSort,
+        searchResultType: resultType,
+        keyword: searchKeyword,
+      })
+    );
+  };
+
   if (loadingStatus) return <div>loading</div>;
   return (
     <div>
-      <Tab resultType={resultType} />
+      <TabSection>
+        <Tab resultType={resultType} />
+        <Select onChange={changeSort} value={sort}>
+          <option value="name asc">가나다 오름차순</option>
+          <option value="name desc">가나다 내림차순</option>
+          <option value="price asc">가격 낮은순</option>
+          <option value="price desc">가격 높은순</option>
+        </Select>
+      </TabSection>
       {searchKeyword.length !== 0 &&
         (searchResultData.length === 0 ? (
           <div>검색 결과가 없습니다</div>
@@ -78,6 +101,26 @@ const SearchResult = (props) => {
     </div>
   );
 };
+const TabSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 35px;
+  background: white;
+  color: gray;
+  padding-left: 5px;
+  font-size: 14px;
+  border: none;
+  margin-left: 10px;
+`;
+const Select = styled.select`
+  height: 35px;
+  background: white;
+  color: ${(props) => props.theme.color.gray3};
+  padding-left: 5px;
+  font-size: 14px;
+  border: none;
+  margin-left: 10px;
+`;
 const TabItem = styled.div`
   background-color: ${(props) => props.active && 'red'};
 `;
