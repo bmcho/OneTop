3db3b49963184d8e323309323508f587c7224eb9
bmcho <bmch91@naver.com>
2022-03-11 20:15:22 +0000

    fix: front- meth:http-equiv 추가, backend- search select 쿼리문 변경

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -3,14 +3,21 @@ import withReduxSaga from 'next-redux-saga';
 import Layout from '../src/components/layout/Layout';
 import ProductCompare from '../src/components/compareBox/ProductCompare';
 import { useRouter } from 'next/router';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
   return (
-    <Layout>
-      <Component {...pageProps} />
-      {router.pathname !== '/' && <ProductCompare />}
-    </Layout>
+    <>
+      <Head>
+        <title>reCco</title>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+        {router.pathname !== '/' && <ProductCompare />}
+      </Layout>
+    </>
   );
 }
 
