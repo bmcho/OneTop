19933e6859fc7481b53e692cb53f9548dc8b7b26
rmsqor103 <rmsqor103@naver.com>
2022-03-11 18:59:32 +0900

    merge:setting

--- front/next.config.js
+++ front/next.config.js
@@ -1,19 +1,10 @@
 module.exports = {
   env: {
-    BASE_URL: 'http://localhost:8000',
-    // BASE_URL: 'http://localhost/api',
+    // BASE_URL: 'http://localhost:8000',
+    BASE_URL: 'http://localhost/api',
   },
   images: {
     domains: ['dn5hzapyfrpio.cloudfront.net'],
   },
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/_error',
-        destination: '/',
-        permanent: false,
-      },
-    ];
-  },
 };
